b420f408d4949c47ccb2eaf4a7c3dfeb9d7c151f
Pawan Rai <1921081+pawankmrai@users.noreply.github.com>
2022-11-20 16:08:51 +0530

    Initial commit
    - Movie demo started

---
+++ MoviesDemo/HomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  MoviesDemo
+//
+//  Created by Pawan on 20/11/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+	// MARK: - Outlets
+	@IBOutlet weak private var collectionView: UICollectionView!
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+	
+	private func prepareData() {
+		
+	}
+
+}
+
---
+++ MoviesDemo/Networking/NetworkManager.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkManager.swift
+//  MoviesDemo
+//
+//  Created by Pawan on 20/11/22.
+//
+
+import Foundation
+
+class NetworkManager {
+	
+}
